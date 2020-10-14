154d5344d85c6c5e8f53015fe59f68d482ceaa85
Alvarado <alvarado.programador@gmail.com>
2020-10-14 11:24:07 -0500

    Estructura inicial del proyecto

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import 'bootstrap'
+import sass from './scss/main.scss'
+require('@fortawesome/fontawesome-free/css/all.min.css')
+require('@fortawesome/fontawesome-free/js/all.min')
+
+import {log} from "./js/functions";
+
+log()
---
+++ webpack.config.js
@@ -0,0 +1,91 @@
+const MiniCssExtractPlugin = require('mini-css-extract-plugin'),
+    HtmlWebpackPlugin = require('html-webpack-plugin'),
+    autoprefixer = require('autoprefixer'),
+    path = require('path'),
+    webpack = require('webpack')
+const {CleanWebpackPlugin} = require('clean-webpack-plugin')
+
+module.exports = {
+    entry: {
+        js: './src/index.js'
+    },
+    output: {
+        filename: "[name].[chunkhash].js",
+        path: path.resolve(__dirname, 'dist')
+    },
+    devtool: "source-map",
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                use: 'babel-loader',
+                exclude: /node_modules/
+            },
+            {
+                test: /\.html$/,
+                use: [
+                    {
+                        loader: "html-loader",
+                        options: {
+                            minimize: true
+                        }
+                    }
+                ]
+            },
+            {
+                test: /\.(css|scss)$/,
+                use: [
+                    'style-loader',
+                    MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    {
+                        loader: 'postcss-loader',
+                        options: {
+                            autoprefixer: {
+                                browser: ['last 2 versions']
+                            },
+                            sourceMap: true,
+                            plugins: () => [ autoprefixer ]
+                        }
+                    },
+                    'resolve-url-loader',
+                    'sass-loader'
+                ]
+            },
+            {
+                test: /\.(jpe?g|png|gif|svg|webp)$/i,
+                loader: 'file-loader',
+                options: {
+                    esModule: false,
+                    name: 'assets/[name].[ext]'
+                }
+            },
+            {
+                test: /\.(ttf|eot|woff2?|mp4|mp3|txt|xml)$/i,
+                use: 'file-loader?name=assets/[name].[ext]'
+            }
+        ]
+    },
+    plugins: [
+        new webpack.ProvidePlugin({
+           $: "jquery",
+           jQuery: "jquery"
+        }),
+        new CleanWebpackPlugin(),
+        new MiniCssExtractPlugin({
+            filename: '[name].[chunkhash].css',
+            chunkFilename: 'id'.css
+        }),
+        new HtmlWebpackPlugin({
+            template: './src/views/template.html',
+            filename: 'index.html',
+            chunks: ['js'],
+            minify: {
+                html5: true,
+                collapseWhitespace: true,
+                caseSensitive: true,
+                removeComments: true
+            }
+        })
+    ]
+}
